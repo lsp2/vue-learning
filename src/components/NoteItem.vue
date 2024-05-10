081d2f474e63153f04584841195a01e86d090149
xsp2331 <1515496430@qq.com>
2024-05-10 13:24:46 +0800

    s

---
+++ src/components/NoteItem.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps(['note'])
+console.log(props.note);
+
+</script>
+
+<template>
+  <div class="item">
+    {{note}}
+  </div>
+</template>
+
+<style scoped>
+.item{
+    
+}
+</style>
---
+++ src/views/NoteList.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import NoteItem from '../components/NoteItem.vue'
+
+const notes = ref([
+    "无事发生",
+    "哈哈哈",
+    "大古变成光"
+])
+</script>
+
+<template>
+    <div>
+        <div v-if="notes.length>0">
+            <div v-for="item in notes" :key="item._id">
+                <NoteItem :note="item" />
+            </div>
+        </div>
+        <button type="">神奇啊</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
