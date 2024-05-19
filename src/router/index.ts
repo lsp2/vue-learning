09223b1451fec27f8374e293cfa90912f92a5b2f
lsp2 <1032574372@qq.com>
2024-05-19 22:00:04 +0800

    start

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import NoteList from '../views/NoteList.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'note',
-      component: NoteList
+      name: 'notelist',
+      component: () => import('../views/NoteList.vue')
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/music',
+      name: 'musicplayer',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/MusicPlayer.vue')
     }
   ]
 })
